901ba7c2137ae51eba6b3dbfd1b3a78de097706f
Asiderr <norkam41@gmail.com>
2019-06-30 12:33:51 -0400

    help me

--- MotorDriver/Src/main.c
+++ MotorDriver/Src/main.c
@@ -22,7 +22,7 @@ int16_t _Rpm;
 uint8_t _TableIndex;
 
 bool _ButtonState = false;
-bool _MotorState;
+uint8_t _ControllerState;
 
 const uint16_t _CurrentTable[118] = {2500, 2541, 2592, 2643, 2694, 2744, 2795, 2846, 2896, 2947, 2947, 2947, 2947, 2947, 2947, 2947, 2947, 2947, 2947,
 		2947, 2947, 2947, 2947, 2947, 2947, 2947, 2926, 2910, 2894, 2877, 2861, 2845, 2828, 2812, 2795, 2779, 2763, 2746,
@@ -31,6 +31,23 @@ const uint16_t _CurrentTable[118] = {2500, 2541, 2592, 2643, 2694, 2744, 2795, 2
 		2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543,
 		2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543,
 		2543, 2543, 2543, 2543 };
+
+const uint16_t _CurrentTable2[118] = {2500, 2541, 2592, 2643, 2694, 2744, 2795, 2846, 2896, 2947, 2947, 2947, 2947, 2947, 2947, 2947, 2947, 2947, 2947,
+		2947, 2947, 2947, 2947, 2947, 2947, 2947, 2926, 2910, 2894, 2877, 2861, 2845, 2828, 2812, 2795, 2779, 2763, 2746,
+		2730, 2714, 2697, 2681, 2665, 2648, 2632, 2615, 2599, 2583, 2566, 2551, 2543, 2543, 2543, 2543, 2543, 2543, 2543,
+		2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543,
+		2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543,
+		2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543,
+		2543, 2543, 2543, 2543 };
+
+const uint16_t _CurrentTable3[118] = {2500, 2541, 2592, 2643, 2694, 2744, 2795, 2846, 2896, 2947, 2947, 2947, 2947, 2947, 2947, 2947, 2947, 2947, 2947,
+		2947, 2947, 2947, 2947, 2947, 2947, 2947, 2926, 2910, 2894, 2877, 2861, 2845, 2828, 2812, 2795, 2779, 2763, 2746,
+		2730, 2714, 2697, 2681, 2665, 2648, 2632, 2615, 2599, 2583, 2566, 2551, 2543, 2543, 2543, 2543, 2543, 2543, 2543,
+		2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543,
+		2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543,
+		2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543, 2543,
+		2543, 2543, 2543, 2543 };
+
 
 
 int main(void)
@@ -58,24 +75,32 @@ int main(void)
 			_AvgSample = acc / MTR_BUFFLEN;
 		}
 
-		//! TESTING PURPOSES!!!!
-		if(BTN_State() || _MotorState)
+
+		if(_ControllerState==0)
 		{
-			if(!_ButtonState)
-			{
-				MTR_TurnOn();
-				_ButtonState = true;
-			}
+
+			MTR_TurnOff();
+
+		}
+		else MTR_TurnOn();
+
+
+		//! TESTING PURPOSES!!!!
+		if(_ControllerState==1) //standard mapa
+		{
+
 			LED_SetLedOn(LED3);
 		}
-		else
+		else if(_ControllerState==2) //turbo mapa
 		{
-			if(_ButtonState)
-			{
-				MTR_TurnOff();
-				_ButtonState = false;
-			}
-			LED_SetLedOff(LED3);
+
+			LED_SetLedOn(LED2);
+		}
+		else if(_ControllerState==3) //?? max current
+		{
+
+			LED_SetLedOn(LED2);
+			LED_SetLedOn(LED3);
 		}
 	}
 }
@@ -90,7 +115,14 @@ void SYS_Tick(void)
 	if(_Rpm >=0) _TableIndex = _Rpm / 40;
 	else _TableIndex = 0;
 	if(_TableIndex >= 118) _TableIndex = 117;
+
+	if(_ControllerState==2)
+	MTR_SetLimit(_CurrentTable2[_TableIndex]);
+	if(_ControllerState==3)
+	MTR_SetLimit(_CurrentTable3[_TableIndex]);
+	else
 	MTR_SetLimit(_CurrentTable[_TableIndex]);
+
 
 	//! TODO: Handle events
 	_LedTim++;
@@ -115,12 +147,15 @@ void ENC_FullRotation(void)
 {
 	_Rotations++;
 }
-void MSG_Received((uint8_t *data, uint8_t len)
+void MSG_Received(uint8_t *data, uint8_t len)
 {
-	if(data[0]==true) _MotorState=true;
+	if(data[0]) _ControllerState=1;
+	else if(data[1]) _ControllerState=2;
+	else if(data[2]) _ControllerState=3;
+	else _ControllerState=0;
 	TIM_reset();
 }
 void TIM_tick(void)
 {
-	_MotorState = false;
+	_ControllerState= 0;
 }
